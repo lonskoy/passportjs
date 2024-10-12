4cb35bf8a13aa8d14318baf8a531d20fd331357b
lonskoy <lonskoy0304@gmail.com>
2024-10-13 00:39:45 +0300

    add socket

--- library/app.js
+++ library/app.js
@@ -1,21 +1,41 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require('cors'); // разрешает запросы к серверу с разных источников
+const bodyParser = require('body-parser'); // преобразовывает POST-запросы отправленные через форму в объект req.body 
 const mongoose = require('mongoose');
-const http = require('http');
-const socketIo = require('socket.io');
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const session = require('express-session')
 const users = require('./db/users.js')
 
+const http = require('http');
+const socketIO = require('socket.io');
+
+
 const booksRouter = require('./routes/booksRouter');
 const booksRouterAPI = require('./routes/booksRouterAPI');
 const userRouter = require('./routes/userRouter.js')
 
-//----------------> Настройка passportJs
+const app = express();
+app.use(express.urlencoded()); // middleware в Express, которое используется для парсинга данных из формы, отправленных с Content-Type: application/x-www-form-urlencoded. Оно превращает данные, отправленные через POST-запрос, в объект req.body, с которым затем можно работать в вашем серверном коде.
+app.use(express.static('public')); // настраиваем статичную папку для загрузки файлов с сервера
 
+const server = http.Server(app);
+const io = socketIO(server);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: process.env.SECRET}));
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api/user', userRouter )
+app.use('/api', booksRouterAPI);
+app.use('/', booksRouter);
+
+//                                      ----------------> Настройка passportJs
 const verify = (username, password, done) => {
   users.findByUsername(username, (err, user) => {
       if (err) {return done(err)}
@@ -46,57 +66,36 @@ passport.deserializeUser( (id, cb) => { //окончание сессии
     cb(null, user)
   })
 })
-
-//<---------------- Настройка passportJs
-
-const app = express();
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(cors());
-app.use(bodyParser.json());
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded());
-app.use(session({ secret: process.env.SECRET}));
-
-app.use(passport.initialize())
-app.use(passport.session())
+//                                       <---------------- Настройка passportJs
 
 
-
-app.use('/api/user', userRouter )
-app.use('/api', booksRouterAPI);
-app.use('/', booksRouter);
-
-app.use(express.static('public')); // настраиваем статичную папку для загрузки файлов с сервера
-app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist/'));
-
-// Socket.IO логика
+//                                       ----------------->Настройка Socket
 io.on('connection', (socket) => {
-    console.log('New client connected');
+  const {id} = socket;
+  console.log(`Socket connected: ${id}`);
 
-    socket.on('joinRoom', (roomId) => {
-        socket.join(roomId);
-        console.log(`User joined room: ${roomId}`);
-    });
+  // работа с комнатами
+  const {roomName} = socket.handshake.query;
+  console.log(`Socket roomName: ${roomName}`);
+  socket.join(roomName);
+  socket.on('sendMessage', ({ roomName, message }) => {
+    io.to(roomName).emit('message', message); // Отправляем сообщение всем в комнате, включая отправителя
+  });
 
-    socket.on('chatMessage', (data) => {
-        const { room, message } = data;
-        io.to(room).emit('message', { message });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+  socket.on('disconnect', () => {
+      console.log(`Socket disconnected: ${id}`);
+  });
 });
+//                                       <---------------- Настройка Socket
+
+
 
 const start = async() => {
     try {
         mongoose.connect(
             process.env.BDCONNECT
         );
+        server.listen(3003)
         console.log('Подключение к БД прошло успешно!')
     } catch (error) {
         console.log(`Ошибка подключения к БД:  ${error}`)
